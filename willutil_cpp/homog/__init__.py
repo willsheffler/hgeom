7301254cb9d5300a4442df1b2e75e360d15f2bd4
Will Sheffler <willsheffler@gmail.com>
2024-05-27 04:24:48 -0700

    remove thgeom, update setup

--- willutil_cpp/homog/__init__.py
+++ willutil_cpp/homog/__init__.py
@@ -1,5 +1,4 @@
-# from willutil_cpp.homog import hgeom
 from willutil_cpp.homog.hgeom import *
-from deferred_import import deferred_import
 
-quat = deferred_import('willutil_cpp.homog.quat')
+
+
--- willutil_cpp/setup.py
+++ willutil_cpp/setup.py
@@ -1,8 +1,26 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
-setup(name='willutil_cpp', version='0.1', url='https://github.com/willsheffler/willutil_cpp', author='Will Sheffler', author_email='willsheffler@gmail.com', description='Wrapped Cpp utils', packages=find_packages(include=['willutil_cpp', 'willutil_cpp.*']), install_requires=[
-   'pytest',
-   'numpy',
-   'cppimport',
-],
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        from willutil_cpp import bvh cluster geom phmap rms util xbin
+
+setup(
+    name='willutil_cpp',
+    version='0.1',
+    url='https://github.com/willsheffler/willutil_cpp',
+    author='Will Sheffler',
+    author_email='willsheffler@gmail.com',
+    description='Wrapped Cpp utils',
+    packages=find_packages(include=['willutil_cpp', 'willutil_cpp.*']),
+    install_requires=[
+        'pytest',
+        'numpy',
+        'cppimport',
+    ],
+   cmdclass={
+        'install': PostInstallCommand,
+    },
 )
